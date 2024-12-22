dfb269293d97eb267df1f3fe5672122391127ebd
MTomasCoro <mcoro@frba.utn.edu.ar>
2024-12-22 19:10:05 -0300

    Actualizo base de datos

--- camisetas.sql
+++ camisetas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-12-2024 a las 20:31:57
+-- Tiempo de generación: 22-12-2024 a las 23:08:09
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -45,6 +45,102 @@ INSERT INTO `camisetas` (`id_camiseta`, `nombre_producto`, `talle_id`, `precio_p
 (3, '24-25 Real Madrid local - Jugador', 3, 40000, NULL),
 (4, '96-97 Liverpool visitante manga larga', 4, 35000, NULL);
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id_categoria` int(11) NOT NULL,
+  `nombre_categoria` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id_categoria`, `nombre_categoria`) VALUES
+(1, 'Hombre'),
+(2, 'Mujer'),
+(3, 'Niños'),
+(4, 'Retro'),
+(5, 'Jugador'),
+(6, 'Fanático'),
+(7, 'Club'),
+(8, 'Selección'),
+(9, 'Primera División Argentina'),
+(10, 'Premier League'),
+(11, 'La Liga'),
+(12, 'Serie A'),
+(13, 'Bundesliga'),
+(14, 'Ligue 1'),
+(15, 'MLS'),
+(16, 'Roshn Saudi League'),
+(17, 'Brasileirao'),
+(18, 'Otras ligas');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id_producto` int(11) NOT NULL,
+  `camiseta_id` int(11) NOT NULL,
+  `talle_id` int(11) NOT NULL,
+  `cantidad_producto` int(11) NOT NULL,
+  `destacado` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id_producto`, `camiseta_id`, `talle_id`, `cantidad_producto`, `destacado`) VALUES
+(5, 1, 1, 10, 0),
+(6, 2, 2, 5, 0),
+(7, 3, 3, 20, 0),
+(8, 4, 4, 7, 0),
+(15, 1, 5, 6, 0),
+(16, 2, 6, 3, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `talles`
+--
+
+CREATE TABLE `talles` (
+  `id_talle` int(11) NOT NULL,
+  `medida` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `talles`
+--
+
+INSERT INTO `talles` (`id_talle`, `medida`) VALUES
+(1, 'XS'),
+(2, 'S'),
+(3, 'M'),
+(4, 'L'),
+(5, 'XL'),
+(6, 'XXL');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Índices para tablas volcadas
 --
@@ -57,6 +153,32 @@ ALTER TABLE `camisetas`
   ADD KEY `talles` (`talle_id`);
 
 --
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `camisetas` (`camiseta_id`),
+  ADD KEY `talles` (`talle_id`);
+
+--
+-- Indices de la tabla `talles`
+--
+ALTER TABLE `talles`
+  ADD PRIMARY KEY (`id_talle`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -65,6 +187,41 @@ ALTER TABLE `camisetas`
 --
 ALTER TABLE `camisetas`
   MODIFY `id_camiseta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT de la tabla `talles`
+--
+ALTER TABLE `talles`
+  MODIFY `id_talle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `camisetas` FOREIGN KEY (`camiseta_id`) REFERENCES `camisetas` (`id_camiseta`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `talles` FOREIGN KEY (`talle_id`) REFERENCES `talles` (`id_talle`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
